e104b02d361c5d33b049139652dede410e75014c
Jeff O'Connell <jeff.oconnell@alumni.iu.edu>
2025-01-04 11:47:31 -0500

    Finish Exercise 2

--- src/app/types.ts
+++ src/app/types.ts
@@ -18,3 +18,8 @@ export interface TvShow {
 }
 
 export type Status = "Canceled/Edited" | "Ended" | "Running" | "New Series" | "To Be Determined";
+
+
+// Added these two types for the Favorites service interface
+export type TvShowId = TvShow["id"]; // This is better than number/more precise We don't just want a number, but we want a type of TvShowID
+export type TvShowIds = Array<TvShowId>; // original: export type TvShowIds = Array<TvShow["id"]>; // This is easier to read. Create plural 
