e7c804d159f2af6c6364a013d658c673c89c6528
Jeff O'Connell <jeff.oconnell@alumni.iu.edu>
2025-01-07 15:35:40 -0500

    Finish Exercixe 3

---
+++ src/app/tv-show-details/tv-show-details.resolver.spec.ts
@@ -0,0 +1,17 @@
+// import { TestBed } from '@angular/core/testing';
+// import { ResolveFn } from '@angular/router';
+
+// import { tvShowDetailsResolver } from './tv-show-details.resolver';
+
+// describe('tvShowDetailsResolver', () => {
+//   const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+//       TestBed.runInInjectionContext(() => tvShowDetailsResolver(...resolverParameters));
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//   });
+
+//   it('should be created', () => {
+//     expect(executeResolver).toBeTruthy();
+//   });
+// });
